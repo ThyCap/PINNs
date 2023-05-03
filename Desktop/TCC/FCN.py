a763c346874a7ac954a4d20f073576434d88c918
Thyago Capitanio <thyagoacapitanio@gmail.com>
2023-05-03 16:21:26 -0300

    firstreorganization

--- Desktop/TCC/FCN.py
+++ Desktop/TCC/FCN.py
@@ -1,13 +1,15 @@
 import torch
 import torch.autograd as autograd         # computation graph
 import torch.nn as nn                     # neural networks
-import torch.optim as optim               # optimizers e.g. gradient descent, ADAM, etc.
+# optimizers e.g. gradient descent,ADAM, etc.
+import torch.optim as optim
 
 from tools import *
+import time
 
 class FCN(nn.Module):
     "Defines a connected network"
-    
+
     def __init__(self, layers, x_domain, x_boundary, y_boundary, partial_diff_equation):
         # super().__init__()
         # activation = nn.Tanh
@@ -21,70 +23,72 @@ class FCN(nn.Module):
         # self.fce = nn.Linear(N_HIDDEN, N_OUTPUT)
 
         # self.loss_function = nn.MSELOSS(reduction = 'mean')
-        super().__init__() #call __init__ from parent class 
+        super().__init__()  # call __init__ from parent class
 
         self.x_domain = x_domain
         self.x_boundary = x_boundary
         self.y_boundary = y_boundary
-              
+
         'activation function'
         self.activation = nn.Tanh()
 
         'loss function'
-        self.loss_function = nn.MSELoss(reduction ='mean')
+        self.loss_function = nn.MSELoss(reduction='mean')
 
         'loss value storage'
         self.loss_bc_history = []
         self.loss_pde_history = []
         self.error_vec_history = []
-    
-        'Initialise neural network as a list using nn.Modulelist'  
-        self.linears = nn.ModuleList([nn.Linear(layers[i], layers[i+1]) for i in range(len(layers)-1)])
-        
+
+        'Initialise neural network as a list using nn.Modulelist'
+        self.linears = nn.ModuleList(
+            [nn.Linear(layers[i], layers[i+1]) for i in range(len(layers)-1)])
+
         self.iter = 0
+        self.startTime = time.time()
+        self.timeHistory = [0]
         self.float()
 
         self.partial_diff_equation = partial_diff_equation
 
         'L-BFGS Optimizer'
-        self.optimizer = optim.LBFGS(self.parameters(), lr, 
-                              max_iter = steps, 
-                              max_eval = None, 
-                              tolerance_grad = 1e-14, 
-                              tolerance_change = 1e-14, 
-                              history_size = steps, 
-                              line_search_fn = 'strong_wolfe')
-    
+        self.optimizer = optim.LBFGS(self.parameters(), lr,
+                                     max_iter=steps,
+                                     max_eval=None,
+                                     tolerance_grad=1e-14,
+                                     tolerance_change=1e-14,
+                                     history_size=steps,
+                                     line_search_fn='strong_wolfe')
+
         'Xavier Normal Initialization'
         for i in range(len(layers)-1):
-            
+
             nn.init.xavier_normal_(self.linears[i].weight.data, gain=1.0)
-            
+
             # set biases to zero
             nn.init.zeros_(self.linears[i].bias.data)
 
-
     def forward(self, x):
-        if torch.is_tensor(x) != True:         
-            x = torch.from_numpy(x)                
-        
+        if torch.is_tensor(x) != True:
+            x = torch.from_numpy(x)
+
         u_b = torch.from_numpy(ub).float()
         l_b = torch.from_numpy(lb).float()
-                      
-        #preprocessing input 
-        x = (x - l_b)/(u_b - l_b) #feature scaling
-        
-        #convert to float
+
+        # preprocessing input
+        x = (x - l_b)/(u_b - l_b)  # feature scaling
+
+        # convert to float
         a = x.float()
-        
+
         for i in range(len(layers)-2):
-            
+
             z = self.linears[i](a)
-                        
+
             a = self.activation(z)
-            
+
         a = self.linears[-1](a)
-        
+
         return a.float()
 
     def loss_BC(self, x_BC, y_BC):
@@ -107,44 +111,50 @@ class FCN(nn.Module):
 
         u = self.partial_diff_equation(f, g)
 
-        u_hat = torch.zeros(x_PDE.shape[0],1)  
+        u_hat = torch.zeros(x_PDE.shape[0], 1)
         u_hat = u_hat.float()
 
         loss = self.loss_function(u, u_hat)
 
         self.loss_bc_history.append(loss)
 
-        return loss 
-    
+        return loss
+
     def loss(self, x_BC, y_BC, x_PDE):
         loss_bc = self.loss_BC(x_BC, y_BC)
         loss_pde = self.loss_PDE(x_PDE)
         return N_f/N_u*loss_bc + loss_pde, loss_bc.item(), loss_pde.item()
 
-    'callable for optimizer'                                       
+    'callable for optimizer'
+
     def closure(self):
         optimizer = self.optimizer
         optimizer.zero_grad()
-        loss, loss_bc, loss_pde = self.loss(self.x_boundary, self.y_boundary, self.x_domain)
+        loss, loss_bc, loss_pde = self.loss(
+            self.x_boundary, self.y_boundary, self.x_domain)
         loss.backward()
 
         self.iter += 1
+        self.timeHistory.append(time.time() - self.startTime)
 
         if self.iter % 50 == 1 or self.iter == 1:
-            print("Iter \t\t Total Loss \t\t Loss per element \t Mean Loss_BC \t\t Mean Loss_PDE")
+            print(
+                "Iter \t\t Total Loss \t\t Loss per element \t Mean Loss_BC \t\t Mean Loss_PDE \t\t Total Elapsed Time (s)")
 
         if self.iter % 5 == 0:
             error_vec, _ = self.test()
-            
-            print("%i \t\t %.3e \t\t %.3e \t\t %.3e \t\t %.3e" % (self.iter, loss.item(),loss.item()/(N_x*N_y), loss_bc, loss_pde))
 
-        return loss 
+            print("%i \t\t %.3e \t\t %.3e \t\t %.3e \t\t %.3e \t\t %.3e" % (
+                self.iter, loss.item(), loss.item()/(N_x*N_y), loss_bc, loss_pde, self.timeHistory[-1]))
+
+        return loss
 
     def test(self):
         u_pred = self.forward(X_test)
         u = torch.zeros(u_pred.shape)
-        error_vec = torch.linalg.norm((u-u_pred),2)      # L2 Norm of the error (Vector)
+        # L2 Norm of the error (Vector)
+        error_vec = torch.linalg.norm((u-u_pred), 2)
         u_pred = u_pred.cpu().detach().numpy()
-        u_pred = np.reshape(u_pred,(100,100),order='F')
-                
-        return error_vec, u_pred 
+        u_pred = np.reshape(u_pred, (100, 100), order='F')
+
+        return error_vec, u_pred
--- Desktop/TCC/Simple Diffusion/PINN.py
+++ Desktop/TCC/Simple Diffusion/PINN.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from PIL import Image
 
 import torch
-import torch.autograd as autograd         # computation graph
+import torch.autograd as autograd
 
 import time
 
@@ -18,27 +18,23 @@ import time
 # u = torch.from_numpy(u_true).float()
 # f_hat = torch.zeros(X_train_Nf.shape[0],1)
 
-# Define the partial differencial equation that drives the loss calculation
+'Neural Network Summary'
+
+
 def partial_diff_equation(f, g):
     f_x_y = autograd.grad(f, g, torch.ones([g.shape[0], 1]), retain_graph=True, create_graph=True)[0]  # first derivative
     f_xx_yy = autograd.grad(f_x_y, g, torch.ones(g.shape), create_graph=True)[0]  # second derivative
 
-    f_yy = f_xx_yy[:, [1]] # we select the 2nd element for y
-    f_xx = f_xx_yy[:, [0]] # we select the 1st element for x
+    f_yy = f_xx_yy[:, [1]] # we select the 2nd element for y (the first one is x) (Remember the input X=[x,y])
+    f_xx = f_xx_yy[:, [0]] # we select the 1st element for x (the second one is y) (Remember the input X=[x,y])
     
-    u = f_xx + f_yy + internalHeatTensor # loss equation
+    u = f_xx + f_yy  # final loss equation
     u = u.float()
 
     return u
 
-# define domain
-X, Y, T = generate_domain()
 
-# define boundary conditions
-
-PINN = FCN(layers, X_train_PDE, X_train_BC, T_train, partial_diff_equation)
-
-'Neural Network Summary'
+PINN = FCN(layers, X_train_Nf, X_train_Nu, T_train, partial_diff_equation)
 print(PINN)
 
 params = list(PINN.parameters())
